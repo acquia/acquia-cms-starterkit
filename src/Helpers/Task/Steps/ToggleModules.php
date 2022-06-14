333674b1a2ca664a316bb7b18077cdd6aa6fce83
Chandan Singh <chandan.singh@acquia.com>
2022-06-14 14:00:16 +0530

    ACMS-1240: Update Starter Kit to have toggle module feature.

---
+++ src/Helpers/Task/Steps/ToggleModules.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AcquiaCMS\Cli\Helpers\Task\Steps;
+
+use AcquiaCMS\Cli\Helpers\Process\Commands\Drush;
+
+/**
+ * Run the drush command to toggle modules based on env.
+ */
+class ToggleModules {
+
+  /**
+   * A drush command object.
+   *
+   * @var \AcquiaCMS\Cli\Helpers\Process\Commands\Drush
+   */
+  protected $drushCommand;
+
+  /**
+   * Constructs an object.
+   *
+   * @param \AcquiaCMS\Cli\Helpers\Process\Commands\Drush $drush
+   *   Holds the drush command class object.
+   */
+  public function __construct(Drush $drush) {
+    $this->drushCommand = $drush;
+  }
+
+  /**
+   * Run the drush commands to toggle modules.
+   *
+   * @param array $args
+   *   An array of params argument to pass.
+   */
+  public function execute(array $args = []) :int {
+    $toggleModulesCommand = ["acms:toggle:modules"];
+    if ($args['no-interaction']) {
+      $toggleModulesCommand = array_merge($toggleModulesCommand, ["--yes"]);
+    }
+    return $this->drushCommand->prepare($toggleModulesCommand)->run();
+  }
+
+}
