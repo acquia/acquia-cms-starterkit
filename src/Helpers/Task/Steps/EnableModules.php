ac751b26199edbe0d3eea0a768500d63e90a0da0
Ankit Pathak <ankit.pathak@acquia.com>
2022-09-19 11:13:16 +0530

    ACMS-1400: Enabling acquia_cms_site_studio after other cms modules.

--- src/Helpers/Task/Steps/EnableModules.php
+++ src/Helpers/Task/Steps/EnableModules.php
@@ -35,9 +35,21 @@ class EnableModules {
    */
   public function execute(array $args = []) :int {
     $modules = $args['modules'];
-    $command = array_merge(["en", "--yes"], $modules);
+    $options = ["en", "--yes"];
+    $is_low_code = FALSE;
+    if (($key = array_search("acquia_cms_site_studio", $modules)) !== FALSE) {
+      $is_low_code = TRUE;
+      unset($modules[$key]);
+    }
+
+    $enable_modules_command = array_merge($options, $modules);
     $args['keys']['UNSET_COHESION_SYNC'] = 1;
-    $this->drushCommand->prepare($command)->run($args['keys']);
+    $this->drushCommand->prepare($enable_modules_command)->run($args['keys']);
+    if ($is_low_code) {
+      $enable_site_studio_command = array_merge($options, ['acquia_cms_site_studio']);
+      $this->drushCommand->prepare($enable_site_studio_command)->run($args['keys']);
+    }
+
     return StatusCodes::OK;
   }
 
