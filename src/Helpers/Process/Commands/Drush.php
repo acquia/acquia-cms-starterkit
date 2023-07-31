4216e0bf539ad4619e8798c9a3863f90557b676e
Rajeshreeputra <pravin.gaikwad@acquia.com>
2023-07-31 12:56:20 +0530

    ACMS-1903: review feedback changes.

--- src/Helpers/Process/Commands/Drush.php
+++ src/Helpers/Process/Commands/Drush.php
@@ -20,9 +20,6 @@ class Drush extends CommandBase {
   protected function getCommand(array $commands): array {
     $uri = $this->input->getParameterOption('--uri');
     $commands = parent::getCommand($commands);
-    if (!empty(shell_exec(sprintf("which %s", escapeshellarg('./vendor/bin/drush'))))) {
-      $commands[0] = './vendor/bin/drush';
-    }
     if ($uri) {
       $commands = array_unique(array_merge($commands, ['--uri=' . $uri]));
     }
